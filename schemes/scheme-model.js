8a238a131a2295e705c5a06c4e61b8b85764e1d8
Bishop <lake.d.bishop@gmail.com>
2019-09-11 16:10:13 -0500

    second model done

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,11 +2,24 @@ const db = require('../data/db-config')
 
 module.exports = {
     find,
+    findById
 }
 
 function find() {
     return db('schemes')
         .then(schemes => {
             return schemes
+        })
+}
+
+function findById(id) {
+    return db('schemes')
+        .where({id : id})
+        .then(scheme => {
+            if (!id || scheme.length == 0) {
+                return null
+            } else {
+                return scheme
+            }
         })
 }
